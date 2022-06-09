01041366a7db9c4b58cc20af0a224f32976f3784
PC-Ngumoha <pcngumoha@gmail.com>
2022-06-09 20:42:07 +0000

    Update shell.c ; Create main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+
+/* helper functions */
+
+
+#endif /* MAIN_H_ */
--- shell.c
+++ shell.c
@@ -1,11 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
+#include "main.h"
 
 /**
  * main - main entry point of the program
