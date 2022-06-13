0c0082defc157070f60e667422e52662c5e8ec94
Nnajinaza <nnajinaza@gmail.com>
2022-06-13 22:07:38 +0000

    Update shell_ops.c and main.h

--- shell_ops.c
+++ shell_ops.c
@@ -9,8 +9,35 @@
  */
 void _list(char **av, char **env)
 {
-	(void) av;
-	(void) env;
+	char *word, *command, *string;
 
-	printf("Operation was successful\n");
+	size_t length1, length2, total_length;
+	pid_t child_id;
+
+	command = strdup(av[0]);
+	length1 = strlen("/bin/");
+	length2 = strlen(command);
+	total_length = length1 + length2 + 1;
+	string = malloc(sizeof(char) * total_length);
+	strcpy(string, "/bin/");
+	strcat(string, command);
+	word = av[0];
+	av[0] = strdup(string);
+	free(word);
+	free(string);
+
+	child_id = fork();
+	if (child_id == -1)
+		dprintf(STDERR_FILENO, "%s: %s\n", av[0], strerror(errno));
+	if (child_id == 0)
+		{
+			if (execve(av[0], av, NULL) == -1)
+				dprintf(STDERR_FILENO, "%s: %s\n", av[0], strerror(errno));
+			free_args(av);
+			_exit(1);
+		}
+	if (child_id > 0)
+		wait(NULL);
+
+	(void) env;
 }
